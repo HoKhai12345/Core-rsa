1ea6c9515581ad25817b671d462897a1c6e51699
khaihq <hoquangkhai231@gmail.com>
2025-08-19 01:55:05 -0700

    Cập nhật code bảng booking

--- src/app/pages/core/booking-location/components/booking-location.component.ts
+++ src/app/pages/core/booking-location/components/booking-location.component.ts
@@ -12,6 +12,24 @@ export class BookingLocationComponent implements OnInit {
     { label: 'Home', nameIcon: 'home', url: '/dashboard' },
     { label: 'Booking location', nameIcon: 'calculator', url: '/booking-location' }
   ]
+
+  timeDayWeek= [
+    {label: 'Thứ 2'},
+    {label: 'Thứ 3'},
+    {label: 'Thứ 4'},
+    {label: 'Thứ 5'},
+    {label: 'Thứ 6'},
+    {label: 'Thứ 7'},
+    {label: 'Chủ nhật'},
+    {label: 'Thứ 2'},
+    {label: 'Thứ 3'},
+    {label: 'Thứ 4'},
+    {label: 'Thứ 5'},
+    {label: 'Thứ 6'},
+    {label: 'Thứ 7'},
+    {label: 'Chủ nhật'},
+  ]
+  timeSlots = ['Sáng', 'Chiều', 'Tối'];
   constructor() { }
 
   ngOnInit(): void {
